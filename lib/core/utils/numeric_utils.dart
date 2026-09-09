d612bf8a06c991df6c7d8324b80b12f0eeecefd3
umar-essayed <omar@zorar.app>
2026-09-09 21:58:15 +0300

    fix(utils): add NumericUtils class with formatCurrency

--- lib/core/utils/numeric_utils.dart
+++ lib/core/utils/numeric_utils.dart
@@ -19,3 +19,17 @@ int parseInt(dynamic value, [int defaultValue = 0]) {
   }
   return defaultValue;
 }
+
+class NumericUtils {
+  static double parse(dynamic value, [double defaultValue = 0.0]) => parseDouble(value, defaultValue);
+  static int parseIntVal(dynamic value, [int defaultValue = 0]) => parseInt(value, defaultValue);
+
+  static String formatCurrency(dynamic amount) {
+    final val = parseDouble(amount);
+    if (val == val.roundToDouble()) {
+      return val.toInt().toString();
+    }
+    return val.toStringAsFixed(2);
+  }
+}
+
